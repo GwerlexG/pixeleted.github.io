9073bc23dbf3e54609765814da5219a7083ef432
Viktor <viktorkrupa99@gmail.com>
2025-04-02 20:36:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+  anime({
+    targets: '.start-div',
+    keyframes: [
+        {gap : "15px"},
+        {gap : '30px'},
+    ],
+    easing: 'easeInOutExpo',
+    duration: 1500,
+    loop: true
+  });
+
+
